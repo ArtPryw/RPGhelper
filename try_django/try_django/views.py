e89178965ed3c013df69a4a77150da0e279eb79e
Artpryw <artur.pryw@gmail.com>
2019-09-27 13:32:32 +0200

    FirstCommit. Django Scaffold

---
+++ try_django/try_django/views.py
@@ -0,0 +1,14 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def home_page(request):
+	my_title = 'Hello There...'
+	return render(request, "home_page.html", {"title":my_title})
+
+def heroes_page(request):
+	return render(request, "heroes_page.html", {"title":"Bohaterowie"})
+
+def game_page(request):
+	return render(request, "game_page.html", {"title":"Rozgrywka"})
+
