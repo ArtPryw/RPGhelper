0bb02771d8259d7d1b69b625925f746f61a6dd79
Artpryw <artur.pryw@gmail.com>
2019-12-10 15:59:08 +0100

    added CSS

--- try_django/settings.py
+++ try_django/settings.py
@@ -117,4 +117,4 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/home/arturbak/Desktop/GitRepos/RPGHelper/RPGhelper/try_django/heroCreator/static/'
