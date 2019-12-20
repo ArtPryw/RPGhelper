a7536409b0b70130601acaeed5f6fb81ab1ee859
Artpryw <artur.pryw@gmail.com>
2019-12-20 13:32:00 +0100

    Added some features in table, and column User

--- try_django/heroCreator/views.py
+++ try_django/heroCreator/views.py
@@ -2,6 +2,7 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.admin.views.decorators import staff_member_required
 from django.shortcuts import render, get_object_or_404
 from django.http import Http404
+from django.http import HttpResponseRedirect
 
 # Create your views here.
 from .models import Hero
@@ -26,13 +27,11 @@ def hero_detail_create_view(request):
         obj.slug = slugCreate.replace(" ","")
         obj.user = request.user
         obj.save()
-        form = HeroCreateModelForm()
-    template_name='form.html'
-    context = {'form':form}
-    return render(request, template_name, context)
-
-
-
+        return HttpResponseRedirect("/heroes")
+    else:
+        template_name='form.html'
+        context = {'form':form}
+        return render(request, template_name, context)
 
 def hero_detail_retrieve_view(request, slug):
     #return one object
@@ -41,18 +40,19 @@ def hero_detail_retrieve_view(request, slug):
     context={"object":obj}
     return render(request, template_name, context)
 
-@login_required
+@login_required 
 def hero_detail_update_view(request, slug):
     obj = get_object_or_404(Hero, slug=slug)
     form = HeroCreateModelForm(request.POST or None, instance=obj)
     template_name='form.html'
-    # DO POPRAWY KONIECZNIE JAK NAJSZYBCIEJ!s
-    if request.user == Hero.user:
+    if request.user == obj.user:
         if form.is_valid():
             obj.slug = str.lower(obj.name + "-" + obj.nickname)
-            form.save()
+            obj.save()
+            return HttpResponseRedirect("/heroes")
     else:
         print("nie masz uprawnień!")
+        #return HttpResponseRedirect("/heroes")
     context={'name':f"Update {obj.name}", "form":form}
     return render(request,template_name,context)
 
@@ -63,6 +63,7 @@ def hero_detail_delete_view(request, slug):
     if request.user == obj.user:
         if request.method == "POST":
                 obj.delete()
+                return HttpResponseRedirect("/heroes")
     else:
         print("nie masz uprawnień do zrobienia tego!")
     form = HeroCreateModelForm()
